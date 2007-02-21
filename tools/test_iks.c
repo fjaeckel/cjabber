6c6d6c21bcb5f8d2ae7848ee8c1daf7b446cb08c
Frederic Jaeckel <jaeckelf@refuzed.org>
2007-02-21 22:24:41 +0100

    bullshit.

--- tools/test_iks.c
+++ tools/test_iks.c
@@ -95,35 +95,23 @@ int main () {
                 break;
             case IKS_NET_NODNS:
                 /* hostname could not be resolved*/
-                if (error("hostname lookup failed") == 1){
-                    iks_disconnect(net.parser);
-                    iks_parser_delete(net.parser);
+                if (error("hostname lookup failed") == 1)
                     return 1;
-                }
             case IKS_NET_NOSOCK:
                 /* socket descriptor cannot be created */
-                if (error("socket cannot be created") == 1){
-                    iks_disconnect(net.parser);
-                    iks_parser_delete(net.parser);
+                if (error("socket cannot be created") == 1)
                     return 1;
-                }
             case IKS_NET_NOCONN:
                 /* 
                  * the connection cannot be established..
                  * this could have several sources.
                  */
-                if (error("connection failed") == 1){
-                    iks_disconnect(net.parser);
-                    iks_parser_delete(net.parser);
+                if (error("connection failed") == 1)
                     return 1;
-                }
             default:
                 /* everything else. */
-                if (error("io error") == 1){
-                    iks_disconnect(net.parser);
-                    iks_parser_delete(net.parser);
+                if (error("io error") == 1)
                     return 1;
-                }
     }
     if (!net.id->resource)
         net.id->resource = RESOURCE;
