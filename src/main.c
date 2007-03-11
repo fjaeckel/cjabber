08dcfafbb03910546562c2014b6d6725247b743c
Frederic Jaeckel <jaeckelf@refuzed.org>
2007-03-11 20:21:50 +0100

    change: released under GPLv2

--- src/main.c
+++ src/main.c
@@ -3,15 +3,22 @@
  * It connects to a server given via cmdline 
  * or per default to jabber@localhost
  *
- * never mind.
- * compile it like:
- *      gcc -o test_iks test_iks.c -liksemel
- * for the debug usage:
- *      gcc -o test_iks test_iks.c -liksemel -DDEBUG
- *
  * author:  Frederic 'jchome' Jaeckel
  * email:   jaeckelf 'at' refuzed 'dot' org
- * license: GPLv2
+ * This file is part of cjabber.
+ *
+ * cjabber is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * cjabber is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cjabber; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <stdio.h>
