6d9f1a100849c797d1f3c0a68112a6a56bb35db7
Frederic Jaeckel <jaeckelf@refuzed.org>
2007-04-04 13:03:44 +0200

    add/change: checks now for errno and changed strncmp away.

--- src/input.c
+++ src/input.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define BUFSIZE 128
 
@@ -41,18 +42,52 @@ char* enter_text(char *text, char *p) {
 
   write(STDOUT_FILENO,text,strlen(text));
   while((ret = read(STDIN_FILENO,buf,BUFSIZE))){ 
-    if(ret == -1)     /* if ret is -1 something failed. */
+    if(ret == -1) {     /* if ret is -1 something failed. */
+      switch(errno) {
+          case EAGAIN:
+              write(STDOUT_FILENO,"O_NONBLOCK specified..",23);
+              break;
+          case EBADF:
+              write(STDOUT_FILENO,"fd is not a valid filedescriptor",33);
+              break;
+          case EFAULT:
+              write(STDOUT_FILENO,"buf is outside our accessible address space.",44);
+              break;
+          case EINTR:
+              write(STDOUT_FILENO,"The call was interrupted by a signal before any data was read.",62);
+              break;
+          case EINVAL:
+              write(STDOUT_FILENO,"opened with O_DIRECT, address is specified in buf or fd is attached to an unsuitable object.",92);
+              break;
+          case EIO:
+              write(STDOUT_FILENO,"I/O Error",9);
+              break;
+          case EISDIR:
+              write(STDOUT_FILENO,"fd refers to a directory",24);
+              break;
+          default:
+              write(STDOUT_FILENO,"WAH! SOMETHING WEIRD HAPPENED!\0",32);
+              break;
+      }
       break;
+    }
     cnt+=ret;         /* increment cnt with the content of ret */
     q=p;              /* assign the backup pointer */
-    p=realloc(p,cnt); /* reallocate the pointer to a fitting memrange */
-    if(p == NULL)     /* if NULL, then it's not allocateable */
-      break;
+    if(buf[cnt] == '\n') {
+        p=realloc(p,cnt-1);
+        break;
+    }
+    else
+        p=realloc(p,cnt); /* reallocate the pointer to a fitting memrange */
+    if(p == NULL){     /* if NULL, then it's not allocateable */
+        free(q);
+        break;
+    }
     strcat(p,buf);    /* append the buf content to p */
 #ifdef DEBUG
     printf("p: %d buf: %d buf: %s\n", strlen(p),strlen(buf),buf);
 #endif
-    if(strncmp(buf,"\n",1)) /* if buf is \n, we don't want to read anymore */
+    if(buf[cnt-1]=='\n')
         break;
   }
 #ifdef DEBUG
